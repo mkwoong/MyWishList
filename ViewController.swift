aa29626819c910063b60059867b7b395436a7dfb
mkwoong <mgu6636@gmail.com>
2024-04-18 19:46:09 +0900

    json parsing, UI에 데이터 연결

--- ViewController.swift
+++ ViewController.swift
@@ -9,14 +9,61 @@ import UIKit
 import CoreData
 
 class ViewController: UIViewController {
-
+    
+    var persistentContainer: NSPersistentContainer? {
+            (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer
+        }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.loadData()
     }
     
+    private func loadData() {
+        let productID = Int.random(in: 1 ... 100)
+        
+        if let url = URL(string: "https://dummyjson.com/products/\(productID)") {
+            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if let error = error {
+                    print("Error: \(error)")
+                } else if let data = data {
+                    do {
+                        let productData = try JSONDecoder().decode(Products.self, from: data)
+                        self.decodeProduct = productData
+                    } catch {
+                        print("Decode Error: \(error)")
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
     
-
-
+    private var decodeProduct: Products? = nil {
+        didSet {
+            guard let decodeProduct = self.decodeProduct else {return}
+            
+            DispatchQueue.main.async {
+                self.productImage.image = nil
+                self.productTitle.text = decodeProduct.title
+                self.productDescription.text = decodeProduct.description
+                self.productPrice.text = "$\(decodeProduct.price)"
+            }
+            
+            DispatchQueue.global().async { [weak self] in
+                if let data = try? Data(contentsOf: decodeProduct.thumbnail), let image = UIImage(data: data) {
+                    DispatchQueue.main.async { self?.productImage.image = image }
+                }
+            }
+        }
+    }
+        
+        @IBOutlet weak var productImage: UIImageView!
+        @IBOutlet weak var productTitle: UILabel!
+        @IBOutlet weak var productDescription: UILabel!
+        @IBOutlet weak var productPrice: UILabel!
+        
+        
+        
 }
-
+    
