8f78aba9515ad42ded4df3cef04f9080de7528a7
mkwoong <mgu6636@gmail.com>
2024-04-18 18:57:46 +0900

    UI빌드 CoreData생성

---
+++ ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MyWishList
+//
+//  Created by 문기웅 on 4/18/24.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+}
+
